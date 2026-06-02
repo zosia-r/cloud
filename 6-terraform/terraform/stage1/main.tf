901c2a03ddbfb715a258b4e14fdd16cb73205c1c
Zofia Różańska <280526@student.pwr.edu.pl>
2026-06-02 20:24:26 +0200

    fix: update RDS security group to allow access from all IPs

--- 6-terraform/terraform/stage1/main.tf
+++ 6-terraform/terraform/stage1/main.tf
@@ -59,7 +59,7 @@ resource "aws_security_group" "rds" {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
-    cidr_blocks = [data.aws_vpc.default.cidr_block]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
